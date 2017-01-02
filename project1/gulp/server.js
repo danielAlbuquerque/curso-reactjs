f190ac959165a870f1f53408502688720f59910d
Daniel Albuquerque <daniel.alb.querque@gmail.com>
2017-01-02 09:54:05 -0300

    processo de transpilação

---
+++ project1/gulp/server.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+	gls = require('gulp-live-server');
+
+gulp.task('server', function() {
+	var sources = [
+		'./dist/**/*.*'
+	];
+
+	var server = gls.static('./', process.env.PORT, process.env.IP);
+	server.start();
+
+	return gulp.watch(sources, function(file) {
+		server.notify.apply(server, [file]);
+	});
+});
+
+gulp.task('serve', ['watch', 'server']);
---
+++ project1/scripts/app.js
@@ -0,0 +1,5 @@
+var minhaDiv = document.getElementById('app');
+React.render(
+    <h1 id="meuTitulo" >Hello World!</h1>,
+    minhaDiv
+);
