ac8a69c03f3ad13bcda2c850c3231f24c63d0818
Daniel Albuquerque <daniel.alb.querque@gmail.com>
2017-01-02 09:59:56 -0300

    es6

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 var minhaDiv = document.getElementById('app');
+
+
 React.render(
     <h1 id="meuTitulo" >Hello World!</h1>,
     minhaDiv
